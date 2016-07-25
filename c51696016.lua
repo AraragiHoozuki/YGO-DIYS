821873e3e07c1f22d800ec210414f1e373d82d03
洛林 <lolingua@mail.ru>
2016-07-25 11:56:33 +0800

    +魔法師梅爾蒂娜

--- c51696016.lua
+++ c51696016.lua
@@ -1,4 +1,86 @@
 --魔法師梅爾蒂娜
 function c51696016.initial_effect(c)
-	
+	--Special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetTarget(c51696016.e1tg)
+	e1:SetOperation(c51696016.e1op)
+	c:RegisterEffect(e1)
+	--summon success
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(51696016,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(c51696016.e2tg)
+	e2:SetOperation(c51696016.e2op)
+	c:RegisterEffect(e2)
+	local e2c=e2:Clone()
+	e2c:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2c)
+	--Level up
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_LVCHANGE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetTarget(c51696016.e3tg)
+	e3:SetOperation(c51696016.e3op)
+	c:RegisterEffect(e3)  
+end
+function c51696016.e1tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c51696016.e1op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) then
+		Duel.SendtoGrave(c,REASON_RULE)
+	end
+end
+
+function c51696016.filter(c,e,tp)
+	return c:GetType()==TYPE_SPELL+TYPE_CONTINUOUS  and c:IsAbleToHand()
+end
+function c51696016.e2tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c51696016.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c51696016.e2op(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c51696016.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function c51696016.e3filter(c)
+	return c:IsRace(RACE_DRAGON)
+end
+function c51696016.e3tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c51696016.e3filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c51696016.e3filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,c51696016.e3filter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c51696016.e3op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	local e1=Effect.CreateEffect(c)
+	if tc:GetLevel()<=11 then
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_LEVEL)
+	e1:SetValue(1)
+	e1:SetReset(RESET_EVENT+0x1ff0000)
+	tc:RegisterEffect(e1)
+	end
 end
